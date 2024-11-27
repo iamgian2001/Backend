d2aff374126841ebb56b25ed17acb5aa5bdef569
Virajith Dissanayaka <121111846+virajithdissanayaka@users.noreply.github.com>
2024-11-28 03:02:23 +0530

    job update

--- src/main/java/com/driveaze/driveaze/service/interfac/JobEntryService.java
+++ src/main/java/com/driveaze/driveaze/service/interfac/JobEntryService.java
@@ -2,6 +2,8 @@ package com.driveaze.driveaze.service.interfac;
 
 import com.driveaze.driveaze.dto.JobEntryDTO;
 import com.driveaze.driveaze.dto.ResponseDTO;
+import com.driveaze.driveaze.entity.JobEntry;
+import org.springframework.data.domain.Page;
 
 public interface JobEntryService {
     ResponseDTO addNewJobEntry(JobEntryDTO jobEntryDTO);
@@ -15,4 +17,6 @@ public interface JobEntryService {
     ResponseDTO getJobEntryById(Integer jobEntryId);
 
     ResponseDTO getTechnicians();
+
+    Page<JobEntry> findJobEntriesWithPaginationAndSorting(int jobId,int offset);
 }
