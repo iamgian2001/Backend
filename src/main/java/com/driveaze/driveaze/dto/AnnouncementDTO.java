d3507acb397062806d5c8787816fdff820fe4a84
SAKKalinduS <sankalpa5285@gmail.com>
2024-12-03 04:01:41 +0530

    Announcement Implementation

--- src/main/java/com/driveaze/driveaze/dto/AnnouncementDTO.java
+++ src/main/java/com/driveaze/driveaze/dto/AnnouncementDTO.java
@@ -1,75 +1,23 @@
 package com.driveaze.driveaze.dto;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnnouncementDTO {
     private int announcementId;
     private String title;
-    private Date date;
+    private LocalDate date;
+    private LocalTime time;
     private String content;
     private String recivers;
 
-    public AnnouncementDTO() {
-    }
 
-    public AnnouncementDTO(int announcementId, String title, Date date, String content, String recivers) {
-        this.announcementId = announcementId;
-        this.title = title;
-        this.date = date;
-        this.content = content;
-        this.recivers = recivers;
-    }
-
-    public int getAnnouncementId() {
-        return announcementId;
-    }
-
-    public void setAnnouncementId(int announcementId) {
-        this.announcementId = announcementId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getRecivers() {
-        return recivers;
-    }
-
-    public void setRecivers(String recivers) {
-        this.recivers = recivers;
-    }
-
-    @Override
-    public String toString() {
-        return "AnnouncementDTO{" +
-                "announcementId=" + announcementId +
-                ", title='" + title + '\'' +
-                ", date=" + date +
-                ", content='" + content + '\'' +
-                ", recivers='" + recivers + '\'' +
-                '}';
-    }
 }
