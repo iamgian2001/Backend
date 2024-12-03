841ad89cff31c16f8b7b7c3e02a4bbbc1d3492f2
Anjani Abeywickrama <anjaniabeywickrama@gmail.com>
2024-12-03 13:01:28 +0530

    Supplier Apayments pagination

--- src/main/java/com/driveaze/driveaze/service/interfac/SupplierService.java
+++ src/main/java/com/driveaze/driveaze/service/interfac/SupplierService.java
@@ -2,6 +2,8 @@ package com.driveaze.driveaze.service.interfac;
 
 import com.driveaze.driveaze.dto.ResponseDTO;
 import com.driveaze.driveaze.dto.SupplierDTO;
+import com.driveaze.driveaze.entity.Supplier;
+import org.springframework.data.domain.Page;
 
 public interface SupplierService {
     ResponseDTO addNewSupplier(SupplierDTO supplierDTO);
@@ -13,4 +15,6 @@ public interface SupplierService {
     ResponseDTO deleteSuppliers(Integer supplierId);
 
     ResponseDTO getSupplierById(Integer supplierId);
+
+    Page<Supplier> findSuppliersWithPaginationAndSorting(int offset);
 }
